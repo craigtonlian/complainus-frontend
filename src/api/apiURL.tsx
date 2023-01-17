3079e17f67197dff502cbc63c16eded68ba2ba50
Craigton Lian <craigton.lian@gmail.com>
2023-01-17 16:09:38 +0800

    updated apiURL

--- src/api/apiURL.tsx
+++ src/api/apiURL.tsx
@@ -1,2 +1,2 @@
-export const API_URL = "https://thawing-spire-36928.herokuapp.com";
+export const API_URL = "https://complainus-backend.herokuapp.com";
 // export const API_URL = "http://localhost:3000";
